9a7b8a543d135532cca8067ffe2a47279c97bd0f
Erwan d'Orgeville <info@erwandorgeville.com>
2020-10-01 16:24:15 -0400

    Answers JSON-RPC requests

--- src/transports/mqtt.test.ts
+++ src/transports/mqtt.test.ts
@@ -2,25 +2,29 @@ import examples from "@open-rpc/examples";
 import { parseOpenRPCDocument } from "@open-rpc/schema-utils-js";
 import { Router } from "../router";
 import MQTTTransport from "./mqtt";
-
+import AsyncMQTT from "async-mqtt"
 // import fetch from "node-fetch";
 import { JSONRPCResponse } from "./server-transport";
 
 describe('mqtt transport', () => {
+  const mqttOptions = {
+    broker: "tcp://localhost:1883",
+    inTopic: "inTopic",
+    outTopic: "outTopic"
+  }
   let transport: MQTTTransport;
+  let mqttClient: AsyncMQTT.AsyncClient;
   beforeAll(async () => {
     const simpleMathExample = await parseOpenRPCDocument(examples.simpleMath);
-    transport = new MQTTTransport({
-      broker: "tcp://localhost:1883",
-      inTopic: "inTopic",
-      outTopic: "outTopic"
-    });
+    transport = new MQTTTransport(mqttOptions);
 
     const router = new Router(simpleMathExample, { mockMode: true });
 
     transport.addRouter(router);
 
     await transport.connect();
+    mqttClient = await AsyncMQTT.connectAsync(mqttOptions.broker)
+    mqttClient.subscribe(mqttOptions.outTopic)
   });
 
   afterAll(() => {
@@ -28,6 +32,23 @@ describe('mqtt transport', () => {
   })
 
   it("can connect to the broker", () => {
-    expect(transport.client.connected).toBeTruthy()
+    expect(transport.client?.connected).toBeTruthy()
+  })
+
+  it("can answer to simple JSON-RPC", (done) => {
+    const messageHandler = (topic: string, payload: Buffer) => {
+      const response = JSON.parse(payload.toString())
+      expect(response.result).toBe(4);
+      mqttClient.off('message', messageHandler)
+      done()
+    }
+    mqttClient.on('message', messageHandler)
+
+    mqttClient.publish(mqttOptions.inTopic, JSON.stringify({
+      id: "0",
+      jsonrpc: "2.0",
+      method: "addition",
+      params: [2, 2],
+    }))
   })
 });
--- src/transports/mqtt.ts
+++ src/transports/mqtt.ts
@@ -17,7 +17,7 @@ export interface MQTTServerTransportOptions {
 
 export default class MQTTServerTransport extends ServerTransport {
   private options: MQTTServerTransportOptions
-  public client: any
+  public client: AsyncMQTT.AsyncClient | null
   // public client: AsyncMQTT.AsyncClient
 
   constructor(options: MQTTServerTransportOptions) {
@@ -28,9 +28,23 @@ export default class MQTTServerTransport extends ServerTransport {
 
   public async connect(): Promise<any> {
     this.client = await AsyncMQTT.connectAsync(this.options.broker);
+    this.client.subscribe(this.options.inTopic)
+    this.client.on('message', (topic: string, payload: Buffer) => {
+      this.mqttRouterHandler(JSON.parse(payload.toString()))
+    })
   }
 
   public end(): void {
-    this.client.end()
+    this.client?.end()
+  }
+
+  private async mqttRouterHandler(payload: any): Promise<void> {
+    let result = null;
+    if (payload instanceof Array) {
+      result = await Promise.all(payload.map((r: JSONRPCRequest) => super.routerHandler(r)));
+    } else {
+      result = await super.routerHandler(payload);
+    }
+    this.client?.publish(this.options.outTopic, JSON.stringify(result))
   }
 }
